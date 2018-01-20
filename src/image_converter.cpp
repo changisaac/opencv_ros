35fe84cbb9bb6df05002a6cf7b5e506fc03b273b
Isaac Chang <chang.isaac97@gmail.com>
2018-01-20 16:42:07 -0500

    Update image_converter.cpp

--- src/image_converter.cpp
+++ src/image_converter.cpp
@@ -27,18 +27,13 @@ public:
   {
     // Subscrive to input video feed and publish output video feed
     image_sub_ = it_.subscribe("/camera/depth/image_rect_raw", 1,
-      &ImageConverter::imageCb, this);
+    &ImageConverter::imageCb, this);
     image_pub_ = it_.advertise("/image_converter/output_video", 1);
-    
-    //window to display for processed image
-    //cv::namedWindow(OPENCV_WINDOW);
   }
 
   //destructor
   ~ImageConverter()
   {
-    //destroy display window
-    //cv::destroyWindow(OPENCV_WINDOW);
   }
 
   
@@ -50,7 +45,6 @@ public:
     {
       //transform depth image to 
       cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_16UC1);//now cv_ptr is the matrix
-      //cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
     }
     catch (cv_bridge::Exception& e)
     {
@@ -59,20 +53,6 @@ public:
     }
 
     depth = cv_ptr->image.at<short int>(cv::Point(240, 320));//you can change 240,320 to your interested pixel
-    
-    //if processing incoming image  
-    /* 
-    // Draw an example circle on the video stream
-    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-      cv::circle(cv_ptr->image, cv::Point(240, 320), 10, CV_RGB(255,0,0));
-    
-    // Update GUI Window
-    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-    cv::waitKey(3);
-    
-    // Output modified video stream
-    image_pub_.publish(cv_ptr->toImageMsg());
-   */ 
   }
 };
 
@@ -94,9 +74,7 @@ int main(int argc, char** argv)
   while (ros::ok())
   {
     std_msgs::Int32 depth_pub;
-    depth_pub.data = ic.depth;
-    //debug:
-    //ROS_INFO("Depth: %d", ic.depth);
+    depth_pub.data = ic.depth;    
     depth_chatter.publish(depth_pub);
     loop_rate.sleep();
     ros::spinOnce();
